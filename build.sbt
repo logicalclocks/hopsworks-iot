b397f1d8d910a952db1427388f228e0cad21eba2
kai-chi <kajetan.maliszewski@gmail.com>
2019-03-06 12:38:50 +0100

    add lwm2m xml models

--- build.sbt
+++ build.sbt
@@ -9,9 +9,11 @@ name := "Hops IoT Gateway"
 addCommandAlias("testc", ";clean;coverage;test;coverageReport")
 
 lazy val akkaVersion = "2.5.21"
+lazy val jettyVersion = "9.4.15.v20190215"
 
 libraryDependencies ++= Seq(
   "org.eclipse.leshan" % "leshan-server-cf" % "1.0.0-M10",
+  "org.eclipse.leshan" % "leshan-server-demo" % "1.0.0-M10",
   "com.github.scopt" %% "scopt" % "4.0.0-RC2",
   "org.apache.avro" % "avro" % "1.8.2",
   "com.typesafe" % "config" % "1.3.3",
@@ -19,6 +21,8 @@ libraryDependencies ++= Seq(
   "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2",
   "ch.qos.logback" % "logback-classic" % "1.1.2",
   "com.typesafe.akka" %% "akka-actor" % akkaVersion,
+  "org.eclipse.jetty" % "jetty-webapp" % jettyVersion,
+  "org.eclipse.jetty" % "jetty-continuation" % jettyVersion,
   "com.typesafe.akka" %% "akka-testkit" % akkaVersion % Test,
   "org.scalatest" %% "scalatest" % "3.0.5" % Test
 )
--- src/main/scala/com/logicalclocks/IotGateway.scala
+++ src/main/scala/com/logicalclocks/IotGateway.scala
@@ -2,10 +2,9 @@ package com.logicalclocks
 
 import akka.actor.ActorRef
 import akka.actor.ActorSystem
-import com.logicalclocks.kafka.HopsKafkaProducer
+import com.logicalclocks.leshan.LeshanActor
+import com.logicalclocks.leshan.LeshanActor.StartServer
 import com.logicalclocks.leshan.LeshanConfig
-import com.logicalclocks.leshan.LeshanServer
-import com.logicalclocks.leshan.LeshanServer.StartServer
 import org.eclipse.leshan.LwM2m
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -27,41 +26,31 @@ object IotGateway extends App {
         .action((x, c) => c.copy(coapsPort = x))
         .text("Set the secure local CoAP port. Default: 5684"),
       help('h', "help")
-        .text("Print the help message")
-    )
+        .text("Print the help message"))
   }
 
   val system: ActorSystem = ActorSystem("iotGateway")
-
 
   val leshanConfig: LeshanConfig = OParser.parse(parser1, args, Config()) match {
     case Some(config) =>
       logger.info(config.toString)
       //      new Thread(new HopsKafkaProducer).start()
-      LeshanConfig(config.coapsHost,
+      LeshanConfig(
+        config.coapsHost,
         config.coapsPort,
-        config.keyStorePath,
-        config.keyStoreType,
-        config.keyStorePass,
-        config.keyStoreAlias,
-        config.keyStoreAliasPass)
+        "",
+        5683)
     case _ =>
       throw new Error("Argument parse error")
   }
 
   val leshanActor: ActorRef =
-    system.actorOf(LeshanServer.props(leshanConfig))
+    system.actorOf(LeshanActor.props(leshanConfig))
 
   leshanActor ! StartServer
 
 }
 
 case class Config(
-                   coapsHost: String = "localhost",
-                   coapsPort: Int = LwM2m.DEFAULT_COAP_SECURE_PORT,
-                   keyStorePath: Option[String] = None,
-                   keyStoreType: Option[String] = None,
-                   keyStorePass: Option[String] = None,
-                   keyStoreAlias: Option[String] = None,
-                   keyStoreAliasPass: Option[String] = None,
-                 )
+  coapsHost: String = "",
+  coapsPort: Int = LwM2m.DEFAULT_COAP_SECURE_PORT)
